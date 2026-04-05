2baecceedaff52a6a973e31017734843c0395d28
Ian Macabulos <iannmacabulos@gmail.com>
2026-04-05 08:47:46 +0800

    fix mobile state

--- src/auth.ts
+++ src/auth.ts
@@ -7,37 +7,33 @@ import { authConfig } from "./auth.config";
 import { z } from "zod";
 import bcrypt from "bcryptjs";
 
-// Validation schema for credentials
 const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  ...authConfig, // Merge with the config
+  ...authConfig, 
   adapter: PrismaAdapter(prisma),
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
-        // 1. Validate Input
         const validatedFields = LoginSchema.safeParse(credentials);
 
         if (validatedFields.success) {
           const { email, password } = validatedFields.data;
 
-          // 2. Check if user exists in DB
           const user = await prisma.user.findUnique({
             where: { email },
           });
 
-          // If no user, or user has no password (meaning they are a Google-only user)
+          // Prevent login if user does not exist or signed up via Google exclusively
           if (!user || !user.password) return null;
 
-          // 3. Check Password
           const passwordsMatch = await bcrypt.compare(password, user.password);
           if (passwordsMatch) return user;
         }
@@ -47,8 +43,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   callbacks: {
-    // Persist the User ID to the session
-    ...authConfig.callbacks, // Keep the authorized callback
+    ...authConfig.callbacks, 
     async session({ session, token }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
